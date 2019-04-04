aa118e3bd076b4108b53491f89211c1e7509c6fe
José Manuel Lucas <josemanuel@jmlweb.es>
2019-04-05 00:31:08 +0200

    snapshots added

--- src/components/Logo/Logo.test.js
+++ src/components/Logo/Logo.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import Logo from './Logo';
 
 describe('Logo', () => {
+  const element = <Logo />;
   test('component renders', () => {
-    const element = <Logo />;
     const wrapper = shallow(element);
     expect(wrapper).toBeTruthy();
+  });
+  test('snapshot', () => {
+    const tree = renderer.create(element).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
